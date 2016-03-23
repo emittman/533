0d9081ae46210b83559d6daa84a2ccc6175ce521
emittman <emittman@iastate.edu>
2016-03-23 18:33:22 -0500

    working is working

--- proj/subset_data.R
+++ proj/subset_data.R
@@ -1,31 +1,28 @@
 library(RSQLite)
-#?dbConnect
 name <- "C:/Users/emittman/GitHub/533/docs_2015/docs_2015/drive_stats.db"
 
+#?dbConnect
 conn <- dbConnect(drv = SQLite(),
           dbname = name)
 
-#q <- query: get distinct serial numbers on units that began testing during 2015
-q <- 'SELECT DISTINCT serial_number, smart_9_raw FROM drive_stats WHERE smart_9_raw < 25'
+#q1 <- query: get distinct serial numbers on units that began testing during 2015
+q1 <- 'SELECT DISTINCT serial_number, smart_9_raw FROM drive_stats WHERE smart_9_raw < 25'
 
-x <- dbGetQuery(conn, q)
+x <- dbGetQuery(conn, q1)
 
-q2 <- paste(c('SELECT serial_number, date, failure, smart_9_normalized FROM drive_stats WHERE serial_number = "', x[3,1], '"'), collapse="")
-y <- dbGetQuery(conn, q2)
 
-q3 <- 'SELECT DISTINCT date FROM drive_stats'
-Z <- dbGetQuery(conn, q3)
-Z[,1] <- as.Date(Z[,1])
-dbDisconnect(conn)
-
-#q4 <- query: return data on those units which began testing in 2015
-q4 <- paste(c('SELECT *
+#q2 <- query: return data on those units which began testing in 2015
+q2 <- paste(c('SELECT *
               FROM drive_stats
               WHERE serial_number IN ("',
               paste(x[,1],collapse='","'),
               '")'), collapse="")
-subset <- dbGetQuery(conn, q4)
+subset <- dbGetQuery(conn, q2)
 subset$date <- as.Date(subset$date)
+
+#disconnect from database
+dbDisconnect(conn)
+
 
 library(plyr)
 
